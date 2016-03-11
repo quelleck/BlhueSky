a358ea460ab18333aa0a0c57c0faf467f665f302
Ethan Seyl <ethan.seyl@hudl.com>
2016-03-11 14:39:55 -0600

    clear config

--- config.py
+++ config.py
@@ -9,14 +9,14 @@
 # Enter your Hue bridge user API key within
 # the quotes. Follow instructions here to get your key:
 # http://www.developers.meethue.com/documentation/getting-started
-hue_user_key = '155119e41f09a34f2651b60c3f5e6cd7'
+hue_user_key = ''
 
 # Enter the MAC address of the bluetooth device
 # that the Pi will scan for within the quotes. If you have
 # more than one device, separate them with spaces within
 # the quotes.
 # Example: 'AA:BB:CC:DD:EE:FF AB:BC:CD:DE:EF'
-device_mac = 'A8:8E:24:5F:3B:23'
+device_mac = ''
 
 # If you'd like to enable IFTTT notifications,
 # replace False with your API key
@@ -29,12 +29,12 @@ ifttt_key = False
 # Set the seconds between bluetooth scans
 # while you're away here. Default is 20
 # seconds.
-seconds_between_bluetooth_scans_when_away = 5
+seconds_between_bluetooth_scans_when_away = 20
 
 # Set the seconds between bluetooth scans
 # while you're away here. Default is 180
 # seconds.
-seconds_between_bluetooth_scans_when_home = 5
+seconds_between_bluetooth_scans_when_home = 180
 
 # Set which light transition happens when you arrive.
 # boring_on:
@@ -47,7 +47,7 @@ seconds_between_bluetooth_scans_when_home = 5
 # police:
 # police_chase:
 # Example: 'loop_each_on'
-option = 'police_chase'
+option = 'loop_each_on'
 
 # Enable sunlight hue adjustments?
 # Default is True.
@@ -59,7 +59,7 @@ sun_tracking = True
 # group 0 - all lights. Use instructions here
 # to find your group numbers:
 # http://www.developers.meethue.com/documentation/getting-started
-group_num = 1
+group_num = 0
 
 # What group should turn off
 # when you leave? Default is group 0,
@@ -68,8 +68,9 @@ group_to_turn_off = 0
 
 # Enter your longitude and latitude in
 # decimal form. Easy to grab from google maps.
-lon = 41.252904
-lat = -95.929074
+# Don't use quotes.
+lon = 41
+lat = -95
 
 # If you don't want to enable sun tracking
 # choose your default color temp.
